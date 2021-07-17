3fffb08a7c191a772d04811ae9db406536b0571e
lcry <173590787@qq.com>
2021-07-17 16:51:10 +0800

    :tada: feat(*): learning-gradle 学习完初次提交

---
+++ README.md
@@ -0,0 +1,4 @@
+## learning-gradle-demo-code
+
+学习 gradle 构建工具demo代码。
+
---
+++ learning-gradle-demo/src/main/java/com/lcry/Test.java
@@ -0,0 +1,16 @@
+package com.lcry;
+
+/**
+ * Test
+ *
+ * @author lcry
+ */
+public class Test {
+    /**
+     *
+     * @param args 参数
+     */
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
